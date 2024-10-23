3b33b340795384cdf34dba471440e2be8cfaad30
NiBienvenu <nicibienvenu@gmail.com>
2024-10-23 08:01:44 +0200

    add pages

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,31 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/property-list', function () {
+    return view('property-list');
+});
+Route::get('/property-type', function () {
+    return view('property-type');
+});
+Route::get('/property-agent', function () {
+    return view('property-agent');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/testimonial', function () {
+    return view('testimonial');
+});
+Route::get('/404', function () {
+    return view('404');
+});
+
