e7aa2e29630206f5208ec470e1f124a09606d330
Jean-Lionel <nijeanlionel@gmailcom>
2024-10-26 16:41:53 +0200

    route

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <!-- Navbar Start -->
 <div class="container-fluid nav-bar bg-transparent">
     <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4">
-        <a href="{{ route('home') }}" class="navbar-brand d-flex align-items-center text-center">
+        <a href="/" class="navbar-brand d-flex align-items-center text-center">
             <div class="icon p-2 me-2">
                 <img class="img-fluid" src="img/icon-deal.png" alt="Icon" style="width: 30px; height: 30px;">
             </div>
@@ -12,7 +12,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto">
-                <a href="{{ route('home') }}" class="nav-item nav-link {{ Request::is('/') ? 'active' : '' }}">Home</a>
+                <a href="/" class="nav-item nav-link {{ Request::is('/') ? 'active' : '' }}">Home</a>
                 <a href="{{ route('about') }}" class="nav-item nav-link {{ Request::is('about') ? 'active' : '' }}">About</a>
                 <div class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle {{ Request::is('property.*') ? 'active' : '' }}" data-bs-toggle="dropdown">Property</a>
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 // Page routes
 Route::controller(PageController::class)->group(function () {
-    Route::get('/', 'home')->name('home');
+    Route::get('/', 'home')->name('/');
 
     Route::get('/about', 'about')->name('about');
 
