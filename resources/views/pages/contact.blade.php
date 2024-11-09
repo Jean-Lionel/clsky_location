371a272d2271d65c7db20b48a905edaae9e9bb23
Jean-Lionel <nijeanlionel@gmailcom>
2024-11-09 09:17:41 +0200

    localisation burundi

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -113,9 +113,10 @@
                     </div>
                     <div class="col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                         <iframe class="position-relative rounded w-100 h-100"
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sParis%2C%20France!5e0!3m2!1sfr!2sfr!4v1603794290143!5m2!1sfr!2sfr"
-                            frameborder="0" style="min-height: 400px; border:0;" allowfullscreen="" aria-hidden="false"
-                            tabindex="0"></iframe>
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d637900.2955227613!2d29.34543!3d-3.380444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMycyMicyOS42IlMgMjknMjEnNDMuNSJF!5e0!3m2!1sfr!2sfr!4v1603794290143!5m2!1sfr!2sfr"
+                        frameborder="0" style="min-height: 400px; border:0;" allowfullscreen="" aria-hidden="false"
+                        tabindex="0"></iframe>
+
                     </div>
                     <div class="col-md-6">
                         <div class="wow fadeInUp" data-wow-delay="0.5s">
