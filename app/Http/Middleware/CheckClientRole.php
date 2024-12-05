34b2ddba552fb9fc849dbe24a4b1819c61264656
Jean-Lionel <nijeanlionel@gmailcom>
2024-12-05 13:44:47 +0200

    check.client

---
+++ app/Http/Middleware/CheckClientRole.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckClientRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+
+        if (Auth::user()->role == 'client') {
+
+            return redirect()->route('/')->with('error', 'Accès non autorisé');
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::controller(PageController::class)->group(function () {
 
 });
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth','check.client'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::resource('users', App\Http\Controllers\UserController::class);
